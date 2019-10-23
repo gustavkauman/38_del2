1e91d7a6c481c2b6d978409cfea43797d4992183
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-23 21:42:06 +0200

    Added switch case to fieldList - method (not done)

--- src/IOOuterActive/Field.java
+++ src/IOOuterActive/Field.java
@@ -2,11 +2,53 @@ package IOOuterActive;
 
 public class Field {
 
+
     public Field(){
 
     }
 
-    public void FieldList(){
+    public void FieldList(int sum){
+
+        switch (sum){
+
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+        }
         
     }
 }
+
+
+/*(Man kan ikke slå 1 med to terninger)
+        Tower         +250
+        Crater         -100
+        Palace gates        +100
+        Cold Desert         -20
+        Walled city         +180
+        Monastery         0
+        Black cave         -70
+        Huts in the mountain         +60
+        The Werewall (werewolf-wall)        -80,    men spilleren får en ekstra tur.
+        The pit         -50
+        Goldmine         +650
+*/
