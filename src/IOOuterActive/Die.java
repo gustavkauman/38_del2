bd5ea7468420b070e6da74f714ad10cfa56f2b31
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-23 20:13:52 +0200

    Added the content of the previous dice-class from first CDIO project to the current die-class

--- src/IOOuterActive/Die.java
+++ src/IOOuterActive/Die.java
@@ -1,4 +1,27 @@
 package IOOuterActive;
 
 public class Die {
+
+    //Intance var.
+    final int maxval = 6; // max value
+    int faceValue;      //current value
+
+
+    //constructor
+    public Die() {
+
+    }
+
+
+    //generates random value between 1 and 6 and returns that value
+    public int roll() {
+
+        faceValue = (int) (Math.random() * maxval) + 1;
+
+        return faceValue;
+
+    }
+
+
+
 }
