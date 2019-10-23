9bf006a170fed3c72f3620176e7cd03cc68f5de1
Moamal <moamal001@gmail.com>
2019-10-23 20:16:37 +0200

    Update Die.java

--- src/IOOuterActive/Die.java
+++ src/IOOuterActive/Die.java
@@ -6,22 +6,13 @@ public class Die {
     final int maxval = 6; // max value
     int faceValue;      //current value
 
-
     //constructor
     public Die() {
-
     }
-
 
     //generates random value between 1 and 6 and returns that value
     public int roll() {
-
         faceValue = (int) (Math.random() * maxval) + 1;
-
         return faceValue;
-
     }
-
-
-
 }
