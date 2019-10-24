76cd18d33718cc1c7f9c1e48aa1a10fb758e12f2
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-24 13:38:58 +0200

    Added a constructor to the Account-class. Also edited the subtractMoney method so that it makes sure the value of balance cannot be negative.

--- src/IOOuterActive/Account.java
+++ src/IOOuterActive/Account.java
@@ -5,6 +5,9 @@ public class Account {
     private int money;
 
 
+    public Account(){
+
+    }
 
 
     //Returns the current balance of money
@@ -21,9 +24,14 @@ public class Account {
     }
 
     //Subtracts the value of the input from the balance and returns a confirmation message
+    //The method also makes sure that the balance cannot be negative.
     public String subtractMoney(int money){
 
+        if(this.money < money)
+            this.money = 0;
+        else
         this.money -= money;
+
         return "Transaktionen er gennemført. Der er hævet " + money + "$ fra din konto";
     }
 
