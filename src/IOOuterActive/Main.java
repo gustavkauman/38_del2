5e0fd8b6e58d4fd80549bd48fcbf27ac91faabeb
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-24 16:51:19 +0200

    Made a small demo showing some abilities of the Player-class. It may inspire the development of the DiceGame-class :)

--- src/IOOuterActive/Main.java
+++ src/IOOuterActive/Main.java
@@ -8,17 +8,23 @@ public class Main {
         Player player1 = new Player();
         Player player2 = new Player();
         Scanner in = new Scanner(System.in);
+
         player1.toStringEnterName();
         player1.setPName(in.nextLine());
-        player1.setPAccount(0);
         player2.toStringEnterName();
         player2.setPName(in.nextLine());
-        player2.setPAccount(0);
+
         in.close();
 
         player1.fieldList(4);
-
         player2.fieldList(2);
+        player1.fieldList(6);
+        player2.fieldList(5);
+        player1.fieldList(8);
+        player2.fieldList(12);
+        player1.fieldList(10);
+        player2.fieldList(9);
+
 
 
 
