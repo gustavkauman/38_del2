f90e8a238e2b9c4cbd94cf1e111cf1242e9222b5
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-24 15:28:37 +0200

    Added the fieldList method to the Player-class and deleted the Field-class Also added the account object as an attribute.

--- src/IOOuterActive/Player.java
+++ src/IOOuterActive/Player.java
@@ -7,11 +7,10 @@ public class Player {
 
     //Atributes
     private String name;
+    private Account account = new Account();
 
-    //Metoder
+
     public Player(){
-
-
 
     }
 
@@ -19,6 +18,7 @@ public class Player {
     public void playGame(){
 
     }
+
 
     //Sets players name via input
     public void setPlayerName(){
@@ -29,6 +29,89 @@ public class Player {
 
         in.close();
     }
+
+    //Sets players account balance to the value of the input
+    public void setPlayerAccount(int amount){
+
+        account.setMoney(amount);
+    }
+
+    //Returns current balance of the players account
+    public int getPlayerbalance(){
+
+        return account.getMoneySum();
+    }
+
+
+    // Checks the value of the input and decides which field the player has landed on
+    // The corresponding amount is either added or withdrawn from the players account
+    public void fieldList(int sum){
+
+        switch (sum){
+
+            case 2:{
+                System.out.println("Tower");
+                System.out.println(account.addMoney(250));          //Tower  +250
+            }
+            break;
+            case 3:{
+                System.out.println("Crater");
+                System.out.println(account.subtractMoney(100));     //Crater   -100
+            }
+            break;
+            case 4:{
+                System.out.println("Palace gates");
+                System.out.println(account.addMoney(100));         //Palace gates  +100
+            }
+            break;
+            case 5:{
+                System.out.println("Cold Desert");
+                System.out.println(account.subtractMoney(20));     //Cold Desert    -20
+            }
+            break;
+            case 6:{
+                System.out.println("Walled city");
+                System.out.println(account.addMoney(180));         //Walled city    +180
+            }
+            break;
+            case 7: {
+                System.out.println("Monastery");
+                System.out.println(account.addMoney(0));           //Monastery    0
+            }
+            break;
+            case 8:{
+                System.out.println("Black cave");
+                System.out.println(account.subtractMoney(70));    //Black cave    -70
+            }
+            break;
+            case 9:{
+                System.out.println("Huts in the mountain");
+                System.out.println(account.addMoney(60));         // Huts in the mountain   +60
+            }
+            break;
+            case 10: {
+                System.out.println("The werewall (werewallwolf-wall)");
+                System.out.println(account.subtractMoney(60));               //The Werewall (werewolf-wall)        -80,
+                                                                             // men spilleren får en ekstra tur.
+            }
+            break;
+            case 11: {
+                System.out.println("The pit");
+                System.out.println(account.subtractMoney(50));     //The pit    -50
+            }
+            break;
+            case 12: {
+                System.out.println("Goldmine");
+                System.out.println(account.addMoney(650));        //Goldmine     +650
+            }
+            break;
+        }
+    }
+
+
+
+
+
 
 
 
