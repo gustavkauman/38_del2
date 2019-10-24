95b9b2be118df6b131a1d5458e26adf4550a35a1
David Mikkelsen <Davidmikkelsen@live.dk>
2019-10-24 13:03:52 +0200

    removed getMoneySum, AddMoney and subtractMoney methods from the Player-class and added them to the Account-class instead.

--- src/IOOuterActive/Account.java
+++ src/IOOuterActive/Account.java
@@ -1,4 +1,32 @@
 package IOOuterActive;
 
 public class Account {
+
+    private int money;
+
+
+
+
+
+
+    //Returns the current balance of the players money
+    public int getMoneySum(){
+
+        return money;
+    }
+
+    //Adds the value of the input to the players balance
+    public void addMoney(int money){
+
+        this.money += money;
+
+    }
+
+    //Subtracts the input from the players balance
+    public void subtractMoney(int money){
+
+        this.money -= money;
+
+    }
+
 }
--- src/IOOuterActive/Player.java
+++ src/IOOuterActive/Player.java
@@ -7,7 +7,6 @@ public class Player {
 
     //Atributes
     private String name;
-    private int money;
 
     //Metoder
     public Player(){
@@ -27,29 +26,6 @@ public class Player {
 
         in.close();
     }
-
-
-    //Returns the current balance of the players money
-    public int getMoneySum(){
-
-        return money;
-    }
-
-    //Adds the value of the input to the players balance
-    public void addMoney(int money){
-
-        this.money += money;
-
-    }
-
-    //Subtracts the input from the players balance
-    public void subtractMoney(int money){
-
-        this.money -= money;
-
-    }
-
-
 
 }
 
