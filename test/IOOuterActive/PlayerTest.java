330b3989999144bb8925c375d47577bf4c5e9970
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-10-28 20:51:47 +0100

    Add T07 JUnit test

--- test/IOOuterActive/PlayerTest.java
+++ test/IOOuterActive/PlayerTest.java
@@ -26,4 +26,23 @@ public class PlayerTest {
         }
 
     }
+
+    @Test
+    public void t07_player_balance_can_be_accessed_and_changed_after_game() {
+        DiceGame dg = new DiceGame();
+        Player p1 = new Player(); Player p2 = new Player();
+
+        dg.initializeGame(p1, p2);
+
+        dg.playGame(p1, p2);
+
+        assertTrue(p1.getPBalance() >= 0);
+        assertTrue(p2.getPBalance() >= 0);
+
+        p1.setPAccount(100);
+        p2.setPAccount(100);
+
+        assertEquals(100, p1.getPBalance());
+        assertEquals(100, p2.getPBalance());
+    }
 }
